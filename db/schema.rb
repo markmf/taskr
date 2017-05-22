814d5fe88c87185b0f07b4266f8fca48f1df5c68
Mark Francisco <markmf@yahoo.com>
2017-05-22 17:24:43 +0900

    Added tasks table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521233014) do
+ActiveRecord::Schema.define(version: 20170522074558) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "content"
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
